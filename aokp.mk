eff00217e06656f03ae651a96e61a24708e52cae
Vatsal <koradiavatsal@hotmail.com>
2016-02-04 15:28:27 -0500

    AOKP-ify

--- aokp.mk
+++ aokp.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
+PRODUCT_NAME := aokp_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
